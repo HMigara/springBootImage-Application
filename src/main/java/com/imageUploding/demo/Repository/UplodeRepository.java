57f83f7d8f714cad545e72243a8824ffc45f1471
Haritha <Harimigara@gmail.com>
2023-11-16 11:36:16 +0530

    first commit

---
+++ src/main/java/com/imageUploding/demo/Repository/UplodeRepository.java
@@ -0,0 +1,7 @@
+package com.imageUploding.demo.Repository;
+
+import com.imageUploding.demo.model.images;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UplodeRepository extends JpaRepository <images,Integer> {
+}
---
+++ src/test/java/com/imageUploding/demo/ImageUplodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imageUploding.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUplodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
